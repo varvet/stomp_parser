88590c53fb3c606305ca7688051a89ef159db480
Kim Burgestrand <kim@burgestrand.se>
2014-02-14 09:11:47 +0100

    Pretty up the benchmark suite

--- spec/bench_helper.rb
+++ spec/bench_helper.rb
@@ -2,6 +2,41 @@ require "bundler/setup"
 require "stomp_parser"
 require "benchmark/ips"
 
+class Benchmark::Suite
+  @current = new
+
+  def self.current
+    @current
+  end
+
+  def initialize
+    @quiet = false
+  end
+
+  def warming(label, warmup_time)
+    print "Warming" unless @warming
+    @warming = true
+    print "."
+  end
+
+  def warmup_stats(warmup_time, cycles_per_100ms)
+  end
+
+  def running(label, time)
+    puts unless @running
+    @running = true
+    puts label
+  end
+
+  def add_report(report, backtrace)
+    puts report.body
+  end
+
+  def quiet?
+    @quiet
+  end
+end
+
 class Benchpress
   attr_reader :options
 
@@ -58,7 +93,7 @@ end
 $__benchmarks__ = []
 
 at_exit do
-  reports = Benchmark.ips(time = 1) do |x|
+  reports = Benchmark.ips(time = 0.5, warmup = 0.5) do |x|
     $__benchmarks__.each do |bench|
       5.times { bench.run_initial }
       x.report(bench.name, &bench)
