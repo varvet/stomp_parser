79bcafac880fd92b5f2e38921e46bb8417c41f17
Kim Burgestrand <kim@elabs.se>
2014-02-26 14:05:37 +0100

    Implement CFrame#to_str in C for a 2x speedup

--- ext/stomp_parser/c_frame.c
+++ ext/stomp_parser/c_frame.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "stomp_parser.h"
 #include <ruby/encoding.h>
 
@@ -10,7 +11,7 @@ ID g_force_encoding;
 VALUE g_content_type;
 VALUE g_content_length_h;
 
-static VALUE frame_unescape_header(VALUE self, VALUE value) {
+static VALUE frame_unescape_header(VALUE value) {
   int length = RSTRING_LEN(value);
   int i = 0, j = 0;
 
@@ -81,11 +82,11 @@ void frame_write_command(VALUE self, VALUE command) {
 void frame_write_header(VALUE self, VALUE key, VALUE value) {
   VALUE headers = rb_ivar_get(self, g_headers);
 
-  key = frame_unescape_header(self, key);
+  key = frame_unescape_header(key);
   VALUE written = Qnil;
 
   if (rb_hash_lookup2(headers, key, Qundef) == Qundef) {
-    written = frame_unescape_header(self, value);
+    written = frame_unescape_header(value);
     rb_hash_aset(headers, key, written);
   }
 }
@@ -116,8 +117,102 @@ void frame_write_body(VALUE self, VALUE body) {
   rb_ivar_set(self, g_body, body);
 }
 
+static VALUE frame_escape_header(VALUE value) {
+  int length = RSTRING_LEN(value);
+  int i = 0, j = 0;
+
+  if (length == 0) {
+    return rb_str_new2("");
+  }
+
+  // If every character needs escaping, we need 2xLENGTH
+  char *result = ALLOC_N(char, (length * 2) + 1);
+  char *source = RSTRING_PTR(value);
+
+  for (i = 0, j = 0; i < length; ++i, ++j) {
+    char chr = source[i];
+
+    switch (chr) {
+      case '\n':
+        result[j] = '\\';
+        result[++j] = 'n';
+        break;
+      case '\r':
+        result[j] = '\\';
+        result[++j] = 'r';
+        break;
+      case ':':
+        result[j] = '\\';
+        result[++j] = 'c';
+        break;
+      case '\\':
+        result[j] = '\\';
+        result[++j] = '\\';
+        break;
+      default:
+        result[j] = chr;
+        break;
+    }
+  }
+
+  VALUE final = rb_enc_str_new(result, j, rb_utf8_encoding());
+  xfree(result);
+  return final;
+}
+
+int build_headers_string(VALUE key, VALUE val, VALUE headers_string) {
+  if (memcmp(RSTRING_PTR(key), "content-length", sizeof("content-length")) == 0) {
+    return ST_CONTINUE;
+  }
+
+  key = frame_escape_header(key);
+  val = frame_escape_header(val);
+  rb_str_buf_cat(headers_string, RSTRING_PTR(key), RSTRING_LEN(key));
+  rb_str_buf_cat(headers_string, ":", 1);
+  rb_str_buf_cat(headers_string, RSTRING_PTR(val), RSTRING_LEN(val));
+  rb_str_buf_cat(headers_string, "\n", 1);
+
+  return ST_CONTINUE;
+}
+
+VALUE frame_serialize(VALUE self) {
+  VALUE command = rb_ivar_get(self, g_command);
+
+  VALUE headers = rb_ivar_get(self, g_headers);
+  VALUE headers_string = Qnil;
+  long headers_length = 0;
+
+  if ( ! RHASH_EMPTY_P(headers)) {
+    headers_string = rb_str_buf_new(0);
+    rb_hash_foreach(headers, build_headers_string, headers_string);
+    headers_length = RSTRING_LEN(headers_string);
+  }
+
+  VALUE body = rb_ivar_get(self, g_body);
+  long body_length = RSTRING_LEN(body);
+
+  char content_length_str[32];
+  long content_length_len = snprintf(content_length_str, sizeof(content_length_str), "content-length:%ld\n", body_length);
+
+  VALUE serialized = rb_str_buf_new(0);
+  rb_str_buf_cat(serialized, RSTRING_PTR(command), RSTRING_LEN(command));
+  rb_str_buf_cat(serialized, "\n", 1);
+  if (headers_length > 0) {
+    rb_str_buf_cat(serialized, RSTRING_PTR(headers_string), headers_length);
+  }
+  rb_str_buf_cat(serialized, content_length_str, content_length_len);
+  rb_str_buf_cat(serialized, "\n", 1);
+  if (body_length > 0) {
+    rb_str_buf_cat(serialized, RSTRING_PTR(body), body_length);
+  }
+  rb_str_buf_cat(serialized, "\0", 1);
+
+  return serialized;
+}
+
 void Init_c_frame(void) {
   VALUE cFrame = rb_const_get(mStompParser, rb_intern("CFrame"));
+  rb_define_method(cFrame, "to_str", frame_serialize, 0);
 
   g_command = rb_intern("@command");
   g_headers = rb_intern("@headers");
--- spec/benchmarks/frame_serializer_bench.rb
+++ spec/benchmarks/frame_serializer_bench.rb
@@ -1,50 +1,58 @@
 require_relative "../bench_helper"
 
-describe "Frame#to_str minimal" do |bench|
-  bench.setup do
-    @frame = StompParser::Frame.new("CONNECT", nil)
+%w[CFrame RubyFrame].each do |frame_klass|
+  frame_klass = begin
+    StompParser.const_get(frame_klass)
+  rescue NameError
+    next
   end
 
-  bench.code { @frame.to_str }
+  describe "#{frame_klass}#to_str minimal" do |bench|
+    bench.setup do
+      @frame = frame_klass.new("CONNECT", nil)
+    end
 
-  bench.assert do |frame_str|
-    frame_str == "CONNECT\ncontent-length:0\n\n\x00"
-  end
-end
+    bench.code { @frame.to_str }
 
-describe "Frame#to_str with header" do |bench|
-  bench.setup do
-    @frame = StompParser::Frame.new("CONNECT", { "heart-beat" => "0,0" }, nil)
-  end
-
-  bench.code { @frame.to_str }
-
-  bench.assert do |frame_str|
-    frame_str == "CONNECT\nheart-beat:0,0\ncontent-length:0\n\n\x00"
-  end
-end
-
-describe "Frame#to_str with headers and small body" do |bench|
-  bench.setup do
-    @frame = StompParser::Frame.new("CONNECT", { "some" => "header" }, "body")
+    bench.assert do |frame_str|
+      frame_str == "CONNECT\ncontent-length:0\n\n\x00"
+    end
   end
 
-  bench.code { @frame.to_str }
+  describe "#{frame_klass}#to_str with header" do |bench|
+    bench.setup do
+      @frame = frame_klass.new("CONNECT", { "heart-beat" => "0,0" }, nil)
+    end
 
-  bench.assert do |frame_str|
-    frame_str == "CONNECT\nsome:header\ncontent-length:4\n\nbody\x00"
+    bench.code { @frame.to_str }
+
+    bench.assert do |frame_str|
+      frame_str == "CONNECT\nheart-beat:0,0\ncontent-length:0\n\n\x00"
+    end
   end
-end
 
-describe "Frame#to_str with headers and large body" do |bench|
-  bench.setup do
-    large_binary = "b\x00" * 2 # make room for headers
-    @frame = StompParser::Frame.new("CONNECT", { "some" => "header" }, large_binary)
+  describe "#{frame_klass}#to_str with headers and small body" do |bench|
+    bench.setup do
+      @frame = frame_klass.new("CONNECT", { "some" => "header" }, "body")
+    end
+
+    bench.code { @frame.to_str }
+
+    bench.assert do |frame_str|
+      frame_str == "CONNECT\nsome:header\ncontent-length:4\n\nbody\x00"
+    end
   end
 
-  bench.code { @frame.to_str }
+  describe "#{frame_klass}#to_str with headers and large body" do |bench|
+    bench.setup do
+      large_binary = "b\x00" * 2 # make room for headers
+      @frame = frame_klass.new("CONNECT", { "some" => "header" }, large_binary)
+    end
 
-  bench.assert do |frame_str|
-    frame_str == "CONNECT\nsome:header\ncontent-length:#{@frame.body.bytesize}\n\n#{@frame.body}\x00"
+    bench.code { @frame.to_str }
+
+    bench.assert do |frame_str|
+      frame_str == "CONNECT\nsome:header\ncontent-length:#{@frame.body.bytesize}\n\n#{@frame.body}\x00"
+    end
   end
 end
